53b51ca7ed765c01ce2ff457bdbe2f8226cb7035
Lukáš Zapletal <lukas@zapletalovi.com>
2022-10-20 11:12:21 +0200

    feat: Introduce GIT_CHANGELOG_REMOTE variable

--- src/git_changelog/build.py
+++ src/git_changelog/build.py
@@ -2,6 +2,7 @@
 
 import datetime
 import sys
+import os
 from subprocess import check_output  # noqa: S404 (we trust the commands we run)
 from typing import Dict, List, Optional, Tuple, Type, Union
 
@@ -242,7 +243,7 @@ class Changelog:
         Returns:
             The origin remote URL.
         """
-        git_url = self.run_git("config", "--get", "remote.origin.url").rstrip("\n")
+        git_url = os.environ.get('GIT_CHANGELOG_REMOTE') or self.run_git("config", "--get", "remote.origin.url").rstrip("\n")
         if git_url.startswith("git@"):
             git_url = git_url.replace(":", "/", 1).replace("git@", "https://", 1)
         if git_url.endswith(".git"):
